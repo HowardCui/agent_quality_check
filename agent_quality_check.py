b1443327be3868b06a9baedfa7b410df269ceb2f
HowardCui <HowardCui@users.noreply.github.com>
2026-02-02 19:56:13 -0500

    ver2_2/2/2026

--- agent_quality_check.py
+++ agent_quality_check.py
@@ -301,15 +301,5 @@ class Agent_quality_check:
             excel_stream=self.excel_url=self.JSON_to_EXCEL()
             self.send_email(excel_stream)
 
-if __name__=="__main__":
-    email_info={
-        "sender_email": 'cuihw@wotransfer.com',
-        "sender_password": "******",
-    }
-
-    bot=Agent_quality_check(
-        email_info=email_info,
-    )
-    bot.run()
 
 
